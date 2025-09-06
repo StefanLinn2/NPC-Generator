e6571bfb9eb88e6857c21d5032d84b835b5315d2
Stefan Linn <stefan.linn.22@gmail.com>
2025-09-05 20:17:53 -0400

    UTF 8 encoding compatible

--- nameSyllables.js
+++ nameSyllables.js
@@ -2,32 +2,33 @@
 
 let syllableData = {
   "openVowels": {
-    "prefix": ["Ae","Eli","Ira","Oa","Uli","Aro","Ena","Io","Oli","Ura","Aea","Ili"],
+    "prefix": ["Ae","Eli","Ira","Oa","Uli","Aro","Ena","Io","Oli","Ura","Aea","Ili","Ka","Lani","Moa","Koa","Ko","Yuki","Haru","Miki","Ayo","Eman","Iman"],
     "middle": ["ia","eo","ua","ai","oa","ie","ua","io","ae","ei","ou","ai","la","ri","na","ta","ka","sa","ma"],
-    "suffix": ["ael","ion","iel","ua","eo","ai","ara","io","ae","iel","aen","iou"]
+    "suffix": ["ael","ion","iel","ua","eo","ai","ara","io","ae","iel","aen","iou","lani","nui","kea","ola","ko","mi","na","ya"]
   },
 
   "harshConsonants": {
-    "prefix": ["Gor","Brak","Thok","Krug","Zul","Vok","Drak","Grum","Krag","Thul","Bor","Drok","Zrak","Khal","Vruk","Ptak","Ktr","Zdr"],
+    "prefix": ["Gor","Brak","Thok","Krug","Zul","Vok","Drak","Grum","Krag","Thul","Bor","Drok","Zrak","Khal","Vruk","Ptak","Ktr","Zdr","Bjor","Thor","Sig","Gud","Ghalib"],
     "middle": ["ak","or","ug","rak","mok","th","ok","urk","az","ok","uk","ar","ik","og","ul","pt","kt","zd","dr","gr","tr"],
-    "suffix": ["th","g","ok","ar","um","ok","ak","or","ug","th","ok","ar","uk","um","oth","zt","kr"]
+    "suffix": ["th","g","ok","ar","um","ok","ak","or","ug","th","ok","ar","uk","um","oth","zt","kr","son","r","ur"]
   },
 
   "softConsonants": {
-    "prefix": ["Lia","Mar","Syla","Nia","Rela","Mina","Fina","Tala","Sera","Nela","Ria","Mara","Lira","Sena","Tira","Ba","Da","Pa","Na","Ma","Ade","Ife"],
-    "middle": ["li","ra","na","si","ma","la","ni","ra","li","ma","sa","ra","li","na","sa","la","na","ra","ma","da","ba","pa"],
-    "suffix": ["el","ia","in","a","il","on","ia","el","in","a","il","on","ea","ia","il","an","en","in","Funmi"]
+    "prefix": ["Lia","Mar","Syla","Nia","Rela","Mina","Fina","Tala","Sera","Nela","Ria","Mara","Lira","Sena","Tira","Ba","Da","Pa","Na","Ma","Ade","Ife","Jean","Marie","Pierre","Fran","Abd","Nur","Abu"],
+    "middle": ["li","ra","na","si","ma","la","ni","ra","li","ma","sa","ra","li","na","sa","la","na","ra","ma","da","ba","pa","al","el","in"],
+    "suffix": ["el","ia","in","a","il","on","ia","el","in","a","il","on","ea","ia","il","an","en","in","Funmi","lise","ette","ah","ullah"]
   },
 
   "complexClusters": {
-    "prefix": ["Str","Gr","Thl","Dr","Kl","Br","Tr","Kr","St","Phl","Bl","Drk","Chl","Gl","Fr","Pt","Kt","Zd","Drk","Trl","Ade"],
+    "prefix": ["Str","Gr","Thl","Dr","Kl","Br","Tr","Kr","St","Phl","Bl","Drk","Chl","Gl","Fr","Pt","Kt","Zd","Drk","Trl","Ade","Bjor","Thor","Sig","Gud","Al","Ibn","bin"],
     "middle": ["an","or","ek","is","ul","ok","ir","el","ar","ok","is","ul","ek","an","or","str","kl","tr","gr","pt","kt","zd"],
-    "suffix": ["ion","ek","ar","in","ul","oth","ek","ar","in","ul","oth","ek","an","or","in"]
+    "suffix": ["ion","ek","ar","in","ul","oth","ek","ar","in","ul","oth","ek","an","or","in","son","dottir"]
   },
 
   "melodic": {
-    "prefix": ["Sio","Oluwa","Ala","Eri","Ona","Lio","Uma","Ira","Ena","Oli","Aea","Uli","Ara","Ela","Ili","Oea","Do","Re","Mi","Fa","Sol","La","Ti","Ife","Ayo"],
-    "middle": ["ia","eo","ua","ai","oa","ie","ua","io","ae","ei","ou","ai","la","ri","na","ma","sa","ta","do","re","mi","fa","sol","la","ti"],
-    "suffix": ["ara","iel","ael","ion","ua","eo","ai","ara","iel","ael","ion","ua","eo","ai","ara","do","re","mi","Funmi"]
+    "prefix": ["Sio","Oluwa","Ala","Eri","Ona","Lio","Uma","Ira","Ena","Oli","Aea","Uli","Ara","Ela","Ili","Oea","Do","Re","Mi","Fa","Sol","La","Ti","Ife","Ayo","Lani","Koa","Ka","Ko","Yuki","Haru","Miki","Jean","Marie","Pierre","Abd","Nur","Abu","Fatimah","Aaliyah","Dominique","Moise"],
+    "middle": ["ia","eo","ua","ai","oa","ie","ua","io","ae","ei","ou","ai","la","ri","na","ma","sa","ta","do","re","mi","fa","sol","la","ti","al","el","in","uddin","ullah"],
+    "suffix": ["ara","iel","ael","ion","ua","eo","ai","ara","iel","ael","ion","ua","eo","ai","ara","do","re","mi","Funmi","lani","ola","ko","mi","na","ya","ette","ique","ah","ullah"]
   }
 };
+
