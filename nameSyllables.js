c56c90098206351eeba561608b934d5b0200eea6
Stefan Linn <stefan.linn.22@gmail.com>
2025-09-06 01:02:52 -0400

    remove redundant syllables

--- nameSyllables.js
+++ nameSyllables.js
@@ -3,32 +3,32 @@
 let syllableData = {
   "openVowels": {
     "prefix": ["Ae","Eli","Ira","Oa","Uli","Aro","Ena","Io","Oli","Ura","Aea","Ili","Ka","Lani","Moa","Koa","Ko","Yuki","Haru","Miki","Ayo","Eman","Iman"],
-    "middle": ["ia","eo","ua","ai","oa","ie","ua","io","ae","ei","ou","ai","la","ri","na","ta","ka","sa","ma"],
-    "suffix": ["ael","ion","iel","ua","eo","ai","ara","io","ae","iel","aen","iou","lani","nui","kea","ola","ko","mi","na","ya"]
+    "middle": ["ia","eo","ua","ai","oa","ie","io","ae","ei","ou","la","ri","na","ta","ka","sa","ma"],
+    "suffix": ["ael","ion","iel","ua","eo","ai","ara","io","aen","iou","lani","nui","kea","ola","ko","mi","na","ya"]
   },
 
   "harshConsonants": {
     "prefix": ["Gor","Brak","Thok","Krug","Zul","Vok","Drak","Grum","Krag","Thul","Bor","Drok","Zrak","Khal","Vruk","Ptak","Ktr","Zdr","Bjor","Thor","Sig","Gud","Ghalib"],
-    "middle": ["ak","or","ug","rak","mok","th","ok","urk","az","ok","uk","ar","ik","og","ul","pt","kt","zd","dr","gr","tr"],
-    "suffix": ["th","g","ok","ar","um","ok","ak","or","ug","th","ok","ar","uk","um","oth","zt","kr","son","r","ur"]
+    "middle": ["ak","or","ug","rak","mok","th","ok","urk","az","uk","ar","ik","og","ul","pt","kt","zd","dr","gr","tr"],
+    "suffix": ["th","g","ok","ar","um","ak","or","ug","uk","oth","zt","kr","son","r","ur"]
   },
 
   "softConsonants": {
     "prefix": ["Lia","Mar","Syla","Nia","Rela","Mina","Fina","Tala","Sera","Nela","Ria","Mara","Lira","Sena","Tira","Ba","Da","Pa","Na","Ma","Ade","Ife","Jean","Marie","Pierre","Fran","Abd","Nur","Abu"],
-    "middle": ["li","ra","na","si","ma","la","ni","ra","li","ma","sa","ra","li","na","sa","la","na","ra","ma","da","ba","pa","al","el","in"],
-    "suffix": ["el","ia","in","a","il","on","ia","el","in","a","il","on","ea","ia","il","an","en","in","Funmi","lise","ette","ah","ullah"]
+    "middle": ["li","ra","na","si","ma","la","ni","sa","da","ba","pa","al","el","in"],
+    "suffix": ["el","ia","in","a","il","on","ea","an","en","funmi","lise","ette","ah","ullah"]
   },
 
   "complexClusters": {
-    "prefix": ["Str","Gr","Thl","Dr","Kl","Br","Tr","Kr","St","Phl","Bl","Drk","Chl","Gl","Fr","Pt","Kt","Zd","Drk","Trl","Ade","Bjor","Thor","Sig","Gud","Al","Ibn","bin"],
-    "middle": ["an","or","ek","is","ul","ok","ir","el","ar","ok","is","ul","ek","an","or","str","kl","tr","gr","pt","kt","zd"],
-    "suffix": ["ion","ek","ar","in","ul","oth","ek","ar","in","ul","oth","ek","an","or","in","son","dottir"]
+    "prefix": ["Str","Gr","Thl","Dr","Kl","Br","Tr","Kr","St","Phl","Bl","Drk","Chl","Gl","Fr","Pt","Kt","Zd","Trl","Ade","Bjor","Thor","Sig","Gud","Al","Ibn","Bin"],
+    "middle": ["an","or","ek","is","ul","ok","ir","el","ar","str","kl","tr","gr","pt","kt","zd"],
+    "suffix": ["ion","ek","ar","in","ul","oth","an","or","son","dottir"]
   },
 
   "melodic": {
     "prefix": ["Sio","Oluwa","Ala","Eri","Ona","Lio","Uma","Ira","Ena","Oli","Aea","Uli","Ara","Ela","Ili","Oea","Do","Re","Mi","Fa","Sol","La","Ti","Ife","Ayo","Lani","Koa","Ka","Ko","Yuki","Haru","Miki","Jean","Marie","Pierre","Abd","Nur","Abu","Fatimah","Aaliyah","Dominique","Moise"],
-    "middle": ["ia","eo","ua","ai","oa","ie","ua","io","ae","ei","ou","ai","la","ri","na","ma","sa","ta","do","re","mi","fa","sol","la","ti","al","el","in","uddin","ullah"],
-    "suffix": ["ara","iel","ael","ion","ua","eo","ai","ara","iel","ael","ion","ua","eo","ai","ara","do","re","mi","Funmi","lani","ola","ko","mi","na","ya","ette","ique","ah","ullah"]
+    "middle": ["ia","eo","ua","ai","oa","ie","io","ae","ei","ou","la","ri","na","ma","sa","ta","do","re","mi","fa","sol","la","ti","al","el","in","uddin","ullah"],
+    "suffix": ["ara","iel","ael","ion","ua","eo","ai","do","re","mi","funmi","lani","ola","ko","mi","na","ya","ette","ique","ah","ullah"]
   }
 };
 
